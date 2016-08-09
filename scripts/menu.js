48866cd104f2b8faa6e4b1ad8387f22805c9e3ba
Saneef Ansari <hello@saneef.com>
2016-08-09 16:48:22 +0530

    adds code to close dropdown when clicked outside the dropdown

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,24 @@
 document.addEventListener('turbolinks:load', function () {
 	const toggleEl = document.querySelectorAll('.js-nav-dropdown-toggle')[0];
-	const menuEl = document.querySelectorAll('.js-nav-dropdown')[0];
+	const menuEl = document.querySelectorAll('.js-main-nav')[0];
+
+	const openMenu = () => menuEl.classList.add('main-nav--open');
+	const closeMenu = () => menuEl.classList.remove('main-nav--open');
 
 	if (toggleEl && menuEl) {
-		toggleEl.addEventListener('click', () => {
-			menuEl.classList.toggle('main-nav__dropdown--open');
+		toggleEl.addEventListener('click', e => {
+			e.stopPropagation();
+			if (menuEl.classList.contains('main-nav--open')) {
+				closeMenu();
+			} else {
+				openMenu();
+			}
+		});
+
+		document.addEventListener('click', () => {
+			if (menuEl.classList.contains('main-nav--open')) {
+				closeMenu();
+			}
 		});
 	}
 });
